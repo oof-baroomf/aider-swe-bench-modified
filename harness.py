6da460729ca392d48f36f5352c1bc4d3bf34eb93
Dhruv Saini <56490747+oof-baroomf@users.noreply.github.com>
2024-08-18 18:36:11 -0700

    Update harness.py

--- harness.py
+++ harness.py
@@ -459,12 +459,13 @@ def main():
     #
     # Configure 1 or more models to use to try and find plausible solutions
     #
+    models = ["deepseek/deepseek-coder"]
     # models = ["openrouter/deepseek/deepseek-chat"]
     # models = ["gpt-4o", "openrouter/anthropic/claude-3-opus"]
     # models = ["openrouter/anthropic/claude-3-opus"]
     # models = ["gpt-4o"]
     # models = ["gpt-4-1106-preview"]
-    models = ["openrouter/anthropic/claude-3.5-sonnet"]
+    # models = ["openrouter/anthropic/claude-3.5-sonnet"]
     # models = ["claude-3-5-sonnet-20240620"]
 
     # How many attempts per model to try and find a plausible solutions?
